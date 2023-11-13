639a0fed2828ac16523550bad4e3f0a1d660fbf7
harshpal09 <harshpal830.com>
2023-11-13 20:06:51 +0530

    first commit

---
+++ ios/Pods/Target Support Files/Pods-Craftslane-CraftslaneTests/Pods-Craftslane-CraftslaneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Craftslane_CraftslaneTests : NSObject
+@end
+@implementation PodsDummy_Pods_Craftslane_CraftslaneTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Craftslane/Pods-Craftslane-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Craftslane : NSObject
+@end
+@implementation PodsDummy_Pods_Craftslane
+@end
